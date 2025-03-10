7a4bf6db8afe7f6e748f3c31b590c3b5f433f20f
sergioaRiverab <sriverabuitrago5@gmail.com>
2025-03-10 09:12:52 -0500

    creacion visual del login y register

--- frontend/universidad-web/src/app/auth/login/login.component.ts
+++ frontend/universidad-web/src/app/auth/login/login.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators,ReactiveFormsModule  } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  formulario!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
+
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  enviarFormulario(): void {
+    if (this.formulario.valid) {
+      console.log('Formulario enviado:', this.formulario.value);
+      
+      // Aquí puedes manejar la lógica de autenticación (por ejemplo, llamar a un servicio de autenticación)
+
+      // Redirigir a la página de inicio después de iniciar sesión
+      this.router.navigate(['/auth/dashboard']); // Cambia esto a la ruta deseada
+
+      // Limpiar el formulario
+      this.formulario.reset();
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
 }
--- frontend/universidad-web/src/app/home/home.component.ts
+++ frontend/universidad-web/src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import { NavbarComponent } from '../shared/components/navbar/navbar.component';
+import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NavbarComponent,NgOptimizedImage,RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
